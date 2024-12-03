bc83a906527d1c1c90a416355c901fa774a6c329
lolbitto <rafa.juck@outlook.com>
2024-12-03 14:32:26 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    	<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel = "stylesheet" href = "models/header.css">
+	<link rel = "stylesheet" href = "style.css">
+    	<title> Central do Ocultismo</title>
+</head>
+<body>
+<?php include "models/header.php"; ?>
+	<div class = "conteudo">
+		<h1> Òbjetivo </h1>
+		<p> O objetivo deste site é armazenar todas as ocorrências sobrenaturais de todo o mundo. Iremos abordar temas de aliens, espiritualidade, maçonaria, thelema, invocações, criaturas, meditações, mantras, magias, personalidades e culturas antigas.</p>
+		<p style = "text-align:center"><img src = "assets/images/cthulhu.jpg" width = "850px"></p>	
+	</div>
+</body>
+</html>
---
+++ models/header.php
@@ -0,0 +1,13 @@
+<body>
+	<header>
+		<img src = "../assets/images/logo.png">
+	
+		<div class = links> 
+			<a href = "aliens/"><p>Aliens</p></a>
+			<a href = "magias/"><p>Magias</p></a>
+			<a href = "criaturas/"><p>Criaturas</p></a>
+			<a href = "mecanismos/"><p>Mecanismos</p></a>
+			<a href = "relatos/"><p>Relatos</p></a>
+		</div>
+	</header>
+</body>
