8f5ea3920a62b62d72fb2df7e66c71e0386a729f
lolbitto <rafae.juck@gmail.com>
2024-12-03 14:50:04 -0300

    Página de relatos

--- relatos/index.php
+++ relatos/index.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    	<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel = "stylesheet" href = "../models/header.css" >
+	<link rel = "stylesheet" href = "style.css">
+	<title>Relatos</title>
+</head>
+<body>
+<?php
+	include "../models/header.php";
+?>
+	
+</body>
+</html>
 
